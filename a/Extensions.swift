2a61d46e19e979944c253968b75553b6c59331c6
Anna <ms.annabell29@gmail.com>
2020-08-11 15:07:06 +0300

    added animations

--- a/Extensions.swift
+++ a/Extensions.swift
@@ -15,8 +15,47 @@ extension UIButton {
         pulse1.fromValue = 1.0
         pulse1.toValue = 1.12
         pulse1.repeatCount = 1
-
+        
         self.layer.add(pulse1, forKey: nil)
+    }
+}
+extension UILabel {
+    func expandLabel() {
+        UIView.animate(withDuration: 0.6, animations: { self.transform = CGAffineTransform(scaleX: 2, y: 2)},
+                       completion: { _ in
+                        UIView.animate(withDuration: 0.6) {
+                            self.transform = CGAffineTransform.identity
+                        }
+        })
+    }
+}
+
+extension UIView {
+    func shake () {
+        let animation = CABasicAnimation(keyPath: "position")
+           animation.duration = 0.0
+           animation.repeatCount = 2
+           animation.autoreverses = true
+           animation.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 20, y: self.center.y))
+           animation.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 20, y: self.center.y))
+
+           self.layer.add(animation, forKey: nil)
+    }
+}
+
+extension UIImageView {
+    enum Direction {
+        case right
+        case left
+    }
+    func flipImageView(direction: Direction) {
+        switch direction {
+        case .right:
+            UIView.transition(with: self, duration: 0.6, options: .transitionFlipFromRight, animations: nil, completion: nil)
+        case .left:
+            UIView.transition(with: self, duration: 0.6, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        }
+        
     }
 }
 struct Animal {
@@ -24,8 +63,11 @@ struct Animal {
        let image: UIImage
    }
 enum VCId: String {
-       case main
-   }
-   enum SBId: String {
-       case main
-   }
+    case main
+}
+enum SBId: String {
+    case main
+}
+enum Constants: Int {
+    case startShift = 50
+}
